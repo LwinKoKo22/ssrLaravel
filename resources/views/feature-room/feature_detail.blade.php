88621483d3fdc4bb577700ab563caf2dda461546
lwinkokointern1 <dkmadsintern1@gmail.com>
2023-03-06 10:14:03 +0630

    fix cameraTesting

--- resources/views/feature-room/feature_detail.blade.php
+++ resources/views/feature-room/feature_detail.blade.php
@@ -16,10 +16,14 @@
            </div>
         </div>
         <div class="row">
-            <div class="col-12 text-center">
-                <button onclick="start_camera()"id="start_camera" class="mt-2"><i class="bi bi-camera-fill"></i></button>
-                <button onclick="take_snapshot()" id="click_btn" class="mt-2" style="display: none;"><i class="bi bi-camera"></i></button>
-                <button onclick="back_camera()" id="rotate_btn" class="mt-2"><i class="bi bi-arrow-clockwise"></i></button>
+            <div class="col-12 ">
+                <div class="">
+                    <div class="d-flex justify-content-center">
+                        <button id="start_camera" class="mt-2 me-2"><i class="bi bi-camera-fill"></i></button>
+                        <button  id="click_btn" class="mt-2 me-2" style="display: none;"><i class="bi bi-camera"></i></button>
+                        <button  id="front_camera" class="mt-2 me-2"><i class="bi bi-arrow-clockwise"></i></button>
+                    </div>
+                </div>
                 {{-- <button onclick="start_camera()" id="reverse_btn" class="mt-2" style="display: none;"><i class="bi bi-arrow-counterclockwise"></i></button> --}}
             </div>
         </div>
@@ -41,18 +45,26 @@
 <script src="https://html2canvas.hertzen.com/dist/html2canvas.min.js"></script>
 <script>
 
-//Front Camera 
-function frontCamera(){
+let camera = document.getElementById('camera');
+let result = document.getElementById('results');
+let start = document.getElementById('start_camera');
+let click = document.getElementById('click_btn');
+let front = document.getElementById('front_camera');
+//To start the task
+start.addEventListener('click',function(){
+    result.innerHTML = "  ";
     Webcam.set({
-   image_format: 'jpeg',
-   jpeg_quality: 90,
-   audio: true, video: { facingMode: { exact: "environment" } }
-});
-Webcam.attach( '#camera' );
-}
+        width: 320,
+        height: 250,
+        image_format: 'jpeg',
+    })
+    Webcam.attach('camera');
+    start.style.display = "none";
+    click.style.display = "block";
+})
 
-//Capture Photo
-function take_snapshot(){
+//Capture Photo Process
+function snapShoot(){
     Webcam.snap( function(data_uri) {
     document.getElementById('results').innerHTML = 
      '<img src="'+data_uri+'" class="responsive"/>';
@@ -63,25 +75,44 @@ function take_snapshot(){
 Webcam.reset();
 }
 
-//To start the task
-function start_camera(){
+//Capture Photo
+click.addEventListener('click',function(){
+    snapShoot();
+})
+
+// function startCamera(){
+//     Webcam.set({
+//    width: 320,
+//    height: 250,
+//    image_format: 'jpeg',
+//    jpeg_quality: 90,
+//    constraints: {
+//     video : true,
+//    }
+// });
+// Webcam.attach( '#camera' );
+// document.getElementById('results').innerHTML = "";
+// document.getElementById('click_btn').style.display = "block";
+// document.getElementById('start_camera').style.display = "none";
+// }
+
+//Front Camera 
+front.addEventListener('click',function(){
+    result.innerHTML = "  ";
     Webcam.set({
-   width: 320,
-   height: 250,
-   image_format: 'jpeg',
-   jpeg_quality: 90,
-   constraints: {
-    video : true,
-   facingMode: 'environment'
-   }
-});
-Webcam.attach( '#camera' );
-document.getElementById('results').innerHTML = "";
-document.getElementById('click_btn').style.display = "block";
-document.getElementById('start_camera').style.display = "none";
-// document.getElementById('reverse_btn').style.display = 'block';
-document.getElementById('rotate_btn').style.display = "block";
-}
+        width: 320,
+        height: 250,
+        image_format: 'jpeg',
+        video: { 
+            facingMode: { 
+                    exact: "environment" 
+                } 
+        }
+    });
+    Webcam.attach( 'camera' );
+    start.style.display = "none";
+    click.style.display = "block";
+})
 
 //File upload
 let upload_file = document.getElementById('upload_file');
@@ -89,17 +120,16 @@ upload_file.addEventListener("change",e=>{
     const file = upload_file.files[0];
     const reader = new FileReader();
     reader.addEventListener('load',()=>{
-       let data = reader.result;
-       document.getElementById('results').innerHTML = 
-     '<img src="'+data+'" />';
-     document.getElementById('click_btn').style.display = "none";
-     Webcam.reset();
-     canvasContainer();
+        let data = reader.result;
+        result.innerHTML ='<img src="'+data+'" />';
+        click.style.display = "none";
+        Webcam.reset();
+        canvasContainer();
     })
-     reader.readAsDataURL(file);
+        reader.readAsDataURL(file);
 })
 
-//download photo 
+// download photo 
 function canvasContainer(){
     html2canvas(document.querySelector("#webcan_container")).then(canvas => {
     document.querySelector('.download').href = canvas.toDataURL();
@@ -108,13 +138,14 @@ function canvasContainer(){
 }
 
 //Back Camera
-function back_camera(){
-document.getElementById('results').innerHTML = "";
-frontCamera();
-document.getElementById('click_btn').style.display = "block";
-document.getElementById('start_camera').style.display = "none";
-document.getElementById('rotate_btn').style.display = "none";
-}
+// function back_camera(){
+//     document.getElementById('results').innerHTML = "";
+//     frontCamera()
+// Webcam.attach( '#camera' );
+// document.getElementById('click_btn').style.display = "block";
+// document.getElementById('start_camera').style.display = "none";
+// document.getElementById('rotate_btn').style.display = "none";
+// }
 
 //reverse button
 // function reverse_camera(){
